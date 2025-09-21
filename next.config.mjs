a9bbe456931253ee493ad378a2ca25b243d34b27
Bukvity Ármin <bukvicarmin@gmail.com>
2025-09-22 00:33:30 +0200

    ignore build errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+    images: {
+      unoptimized: true,
+    },
+    async headers() {
+      return [
+        {
+          source: '/:all*(svg|jpg|jpeg|png|webp|gif)',
+          locale: false,
+          headers: [
+            {
+              key: 'Cache-Control',
+              value: 'public, max-age=31536000, immutable',
+            },
+          ],
+        }, 
+      ];
+    },
+  };
+  
+  export default nextConfig;
